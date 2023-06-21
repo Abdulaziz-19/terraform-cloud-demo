4f6619f617a3df2333574473f22b70679e5d692b
Abdulaziz <121072628+Abdulaziz-19@users.noreply.github.com>
2023-06-20 19:46:53 -0500

    added rds

--- rds/main.tf
+++ rds/main.tf
@@ -10,5 +10,5 @@ data "terraform_remote_state" "vpc" {
 }
 
 resource "aws_db_subnet_group" "default" {
-  name       = "main"
+  name       = "project_db_subnet_group"
   subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
